86dcd78d380ba0b6338a3587b6e89505dade338b
metiu07 <metiu07@gmail.com>
2014-10-10 20:22:22 +0200

    Refactoring

--- 8259.h
+++
@@ -1,6 +0,0 @@
-#ifndef INCLUDE_2859
-#define INCLUDE_2859
-
-void init8259();
-
-#endif
--- io.h
+++
@@ -1,7 +0,0 @@
-#ifndef IO
-#define IO
-
-inline unsigned char inportb(unsigned int port, unsigned char value);
-inline void outportb(unsigned char port);
-
-#endif
