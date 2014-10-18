05da7c087c8064dc3b5494d2614db256e1b52923
metiu07 <metiu07@gmail.com>
2014-10-18 17:40:35 +0200

    Refactoring

---
+++ src/gdt.c
@@ -0,0 +1,20 @@
+struct gdt_entry
+{
+	unsigned short limit_low;
+	unsigned short base_low;
+	unsigned char base_middle;
+	unsigned char access;
+	unsigned char granularity;
+	unsigned char base_high;
+} __attribute__((packed));
+
+struct gtd_ptr
+{
+	unsigned short limit;
+	unsigned int base;
+} __attribute__((packed));
+
+struct gdt_entry gdt[3];
+struct gdt_ptr gp;
+
+extern void gdt_flush();
